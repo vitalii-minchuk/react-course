7d3802382eddf78ae5b366ad203786820790dbd4
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-04-07 02:31:36 +0300

    add new app

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img className='logo' src='https://www.pngplay.com/wp-content/uploads/13/Freepnglogos-Free-PNG.png' />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+        <ul>
+          <li>
+            <a href='#'>Profile</a>
+          </li>
+          <li>
+            <a href='#'>Message</a>
+          </li>
+          <li>
+            <a href='#'>News</a>
+          </li>
+          <li>
+            <a href='#'>Music</a>
+          </li>
+          <li>
+            <a href='#'>Settings</a>
+          </li>
+        </ul>
+      </nav>
+  );
+};
+
+export default Navbar;
