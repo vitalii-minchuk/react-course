a2c263922c4eb4e77723b2a3787284e0672107ee
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-05-08 17:31:21 +0300

    eseEffect practic

--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -3,29 +3,135 @@ import React, { useEffect, useState } from "react"
 // @ts-ignore
 import s from "./Music.module.css"
 
-type UserType = {
-  login: string
-  id: number
-  avatar_url: string
-  followers: number
+export const Search = (props: SearchPropsType) => {
+  const [tempSearch, setTempSearch] = useState("")
+
+  useEffect(() => {
+    setTempSearch(props.value)
+  }, [props.value])
+
+  return (
+    <div>
+      <input
+        placeholder="search"
+        value={tempSearch}
+        onChange={(e) => { setTempSearch(e.currentTarget.value) }}
+      />
+      <button onClick={() => { props.onSubmit(tempSearch) }} >find</button>
+    </div>
+  )
 }
 
-type SearchUserType = {
-  login: string
-  id: number
+export const UsersList = (props: UsersListPropsType) => {
+  const [users, setUsers] = useState<SearchUserType[]>([])
+
+  useEffect(() => {
+    axios.get<SearchResult>(`https://api.github.com/search/users?q=${props.term}`)
+      .then(res => {
+        setUsers(res.data.items.filter((m, index, arr) => index >= arr.length - 11))
+      })
+  }, [props.term])
+
+  useEffect(() => {
+    if (props.selectedUser === null) {
+      setUsers([])
+    }
+  }, [props.selectedUser])
+
+  return (
+    <ul className={s.list}>
+      {users.map(el => <li key={el.id} onClick={() => {
+        props.onUserSelect(el)
+      }} className={props.selectedUser === el ? s.selected : ""}>{el.login}</li>)}
+    </ul>
+  )
 }
 
-type SearchResult = {
-  items: Array<SearchUserType>
+const startTimerValue = 20
+
+export const UsersDetails = (props: UsersDetailsPropsType) => {
+  const [userDetails, setUserDetails] = useState<UserType | null>(null)
+  const [seconds, setSeconds] = useState(startTimerValue)
+
+  useEffect(() => {
+    if (!!props.user) {
+      axios.get<UserType>(`https://api.github.com/users/${props.user.login}`)
+        .then(res => {
+          setSeconds(startTimerValue)
+          setUserDetails(res.data)
+        }
+      )
+    }
+  }, [props.user])
+
+  useEffect(() => {
+    if(seconds < 1) {
+      setUserDetails(null)
+    }
+  }, [seconds])
+
+  useEffect(() => {
+    if (props.selectedUser === null) {
+      setUserDetails(null)
+    }
+  }, [props.selectedUser])
+
+  return (
+    <div>
+      <div className={s.topBox}>
+        <h3>{userDetails?.login}</h3>
+        {userDetails &&
+          <Timer
+            timerKey={userDetails.id.toString()}
+            seconds={seconds}
+            onChange={setSeconds}
+          />
+        }
+      </div>
+      {userDetails && <div>
+        <img className={s.img} src={userDetails?.avatar_url} alt="avatar" />
+        <p>followers:{userDetails?.followers}</p>
+      </div>}
+    </div>
+  )
 }
 
+export const Timer = (props: TimerPropsType) => {
+  const [seconds, setSeconds] = useState(props.seconds)
+
+  useEffect(() => {
+    setSeconds(props.seconds)
+  }, [props.seconds])
+
+  useEffect(() => {
+    props.onChange(seconds)
+  }, [seconds])
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((actualSec) => actualSec - 1)
+    }, 1000)
+
+    return () => { clearInterval(intervalId) }
+    
+  }, [props.timerKey])
+
+  return (
+    <div>
+      <h5>{seconds}</h5>
+    </div>
+  )
+}
 
 const MusicPage = () => {
   const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
-  const [userDetails, setUserDetails] = useState<UserType | null>(null)
-  const [users, setUsers] = useState<SearchUserType[]>([])
-  const [tempSearch, setTempSearch] = useState("")
+
   const [searchTerm, setSearchTerm] = useState("")
+
+  const resetHandel = () => {
+    setSelectedUser(null)
+    setSearchTerm("")
+  }
 
   useEffect(() => {
     if (selectedUser) {
@@ -33,52 +139,54 @@ const MusicPage = () => {
     }
   }, [selectedUser])
 
-  useEffect(() => {
-    axios.get<SearchResult>(`https://api.github.com/search/users?q=${searchTerm}`)
-    .then(res => {
-      setUsers(res.data.items.filter((m, index, arr) => index >= arr.length - 12))
-    })
-  }, [searchTerm])
-
-  useEffect(() => {
-    if (!!selectedUser) {
-      axios.get<UserType>(`https://api.github.com/users/${selectedUser.login}`)
-        .then(res => {
-          setUserDetails(res.data)
-        }
-      )
-    }
-  }, [selectedUser])
-
   return (
     <>
       <h2 className={s.title}>Music</h2>
-      <div>
-        <input
-          placeholder="search"
-          value={tempSearch}
-          onChange={(e) => {setTempSearch(e.currentTarget.value)}}
-        />
-        <button onClick={() => {
-          setSearchTerm(tempSearch)
-        }} >find</button>
-      </div>
+      <Search value={searchTerm} onSubmit={(value) => { setSearchTerm(value) }} />
+      <button onClick={resetHandel}
+      >reset</button>
       <div className={s.box}>
-        <ul className={s.list}>
-          {users.map(el => <li key={el.id} onClick={() => {
-            setSelectedUser(el)
-          }} className={selectedUser === el ? s.selected : ""}>{el.login}</li>)}
-        </ul>
-        <div>
-          <h3>{selectedUser?.login}</h3>
-          {selectedUser && <div>
-            <img src={userDetails?.avatar_url} alt="avatar" />
-            <p>followers:{userDetails?.followers}</p>
-          </div>}
-        </div>
+        <UsersList
+          term={searchTerm}
+          selectedUser={selectedUser}
+          onUserSelect={(user) => { setSelectedUser(user) }}
+        />
+        <UsersDetails user={selectedUser} selectedUser={selectedUser} />
       </div>
     </>
   )
+}
+
+type UserType = {
+  login: string
+  id: number
+  avatar_url: string
+  followers: number
+}
+type SearchUserType = {
+  login: string
+  id: number
+}
+type SearchResult = {
+  items: Array<SearchUserType>
+}
+type SearchPropsType = {
+  value: string
+  onSubmit: (fixedValue: string) => void
+}
+type UsersListPropsType = {
+  term: string
+  selectedUser: SearchUserType | null
+  onUserSelect: (user: SearchUserType) => void
+}
+type UsersDetailsPropsType = {
+  user: SearchUserType | null
+  selectedUser: SearchUserType | null
+}
+type TimerPropsType = {
+  seconds: number
+  onChange: (actualSec: number) => void
+  timerKey: string
 }
 
 export default MusicPage
